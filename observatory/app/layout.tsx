877881d281bf574aebe3aa1e9d639cd72d08366f
Pranay Joshi <pjoshi6@crimson.ua.edu>
2026-08-28 13:24:33 -0500

    Add Microsoft Clarity analytics

--- observatory/app/layout.tsx
+++ observatory/app/layout.tsx
@@ -43,6 +43,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "y9khep9f7w");`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
